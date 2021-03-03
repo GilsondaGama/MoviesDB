816af0b93bf942f6c787b049168288fb8c30ddce
Gilson da Gama <gilsondagama@hotmail.com>
2021-03-03 16:31:53 -0300

    Atividade 02 - layout do formulário identico ao modelo

--- Atividade02/public/contato/index.php
+++ Atividade02/public/contato/index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>--Formulário Contato--</title>
+  <title>Formulário Contato</title>
   <meta charset="utf-8" />
   <link href="style.css" rel="stylesheet" media="all" />
 </head>
@@ -11,15 +11,6 @@
   <form name="meu_form">
 
     <h2>Formulario de Contato</h2>
-
-
-    <input type="radio" id="male" name="gender" value="male">
-    <label for="male">Masculino</label>
-    <input type="radio" id="female" name="gender" value="female">
-    <label for="female">Feminino</label><br>
-
-
-
 
     <table>
       <tbody>
@@ -38,7 +29,7 @@
         <tr>
           <td align="right">Opções</td>
           <td>
-            <select id="cars" name="cars">
+            <select id="itens" name="itens">
               <option value="1">item_1 da lista</option>
               <option value="2">item_2 da lista</option>
               <option value="3">item_3 da lista</option>
@@ -47,6 +38,16 @@
           </td>
         </tr>
 
+        <tr>
+          <td align="right">Sexo</td>
+          <td>
+            <input type="radio" id="male" name="sexo" value="masculino">
+            <label for="masculino">Masculino</label>
+            <input type="radio" id="female" name="sexo" value="feminino">
+            <label for="feminino">Feminino</label>
+          </td>
+        </tr>
+        <tr>
 
         <tr>
           <td align="right">Mensagem</td>
